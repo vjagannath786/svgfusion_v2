960ae73211ae08042068ce45f93cbb6b7126e4a2
vjagannath786 <36917923+vjagannath786@users.noreply.github.com>
2025-09-15 00:17:59 +0530

    Update __init__.py

--- svgutils/__init__.py
+++ svgutils/__init__.py
@@ -1,6 +1,6 @@
 from .svgparsing import SVGParser
 from .svgtensor import SVGToTensor_Normalized
-from .dataset_preparation_v2 import DynamicProgressiveSVGDataset
+from .datasetpreparation_v5 import DynamicProgressiveSVGDataset, load_dino_model_components
 from .tensorsvg import tensor_to_svg_file_hybrid_wrapper
 
-__all__ = ["SVGParser", "SVGToTensor_Normalized", "DynamicProgressiveSVGDataset", "tensor_to_svg_file_hybrid_wrapper"]
+__all__ = ["SVGParser", "SVGToTensor_Normalized", "DynamicProgressiveSVGDataset", "tensor_to_svg_file_hybrid_wrapper", "load_dino_model_components"]
